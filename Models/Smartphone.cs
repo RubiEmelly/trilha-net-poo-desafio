86aca79fc807cd02620be2e5e285d9a7cf00e96b
RubiEmelly <emellyandrade.rubiyasmin@gmail.com>
2024-09-17 11:43:15 -0300

    Desafio POO concluído com sucesso

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -3,24 +3,32 @@ namespace DesafioPOO.Models
     public abstract class Smartphone
     {
         public string Numero { get; set; }
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        private string Modelo { get; set; }
+        private string IMEI { get; set; }
+        private int Memoria { get; set; }
 
-        public Smartphone(string numero)
+        // Construtor com todos os parâmetros
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
-            // TODO: Passar os parâmetros do construtor para as propriedades
+            Modelo = modelo;
+            IMEI = imei;
+            Memoria = memoria;
         }
 
+        // Método para ligar o smartphone
         public void Ligar()
         {
             Console.WriteLine("Ligando...");
         }
 
+        // Método para receber ligação
         public void ReceberLigacao()
         {
             Console.WriteLine("Recebendo ligação...");
         }
 
+        // Método abstrato para instalar aplicativo
         public abstract void InstalarAplicativo(string nomeApp);
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,29 @@
-﻿using DesafioPOO.Models;
+﻿using System;
+using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+namespace DesafioPOO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Criar uma instância da classe Nokia
+            Nokia nokia = new Nokia("123456789", "Nokia 3310", "NOK123456", 64);
+            
+            // Criar uma instância da classe Iphone
+            Iphone iphone = new Iphone("987654321", "iPhone 13", "IPH123456", 128);
+
+            // Testar os métodos da classe Nokia
+            Console.WriteLine("Smartphone Nokia:");
+            nokia.Ligar();
+            nokia.InstalarAplicativo("WhatsApp");
+            Console.WriteLine(); 
+
+            // Testar os métodos da classe Iphone
+            Console.WriteLine("Smartphone iPhone:");
+            iphone.ReceberLigacao();
+            iphone.InstalarAplicativo("Instagram");
+            Console.WriteLine(); 
+        }
+    }
+}
